6cb3be0563b160b4559deee91e32378d3652f475
Alexander Ryzhikov <coobaha@gmail.com>
2018-10-20 18:02:41 +0300

    travis.yml

---
+++ ci/travis_before_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+case "$TRAVIS_OS_NAME" in
+    osx)
+        brew update
+        brew install yarn
+        ;;
+    linux)
+        sudo apt-key adv --fetch-keys http://dl.yarnpkg.com/debian/pubkey.gpg
+        echo "deb http://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+        sudo apt-get update -qq
+        sudo apt-get install -y -qq yarn
+        ;;
+esac
